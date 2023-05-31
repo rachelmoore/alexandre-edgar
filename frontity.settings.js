e53559298d77bf25a1b5eacffaa5cda59bd104bc
Rachel Moore <rachel@rachelmoore.net>
2023-05-31 12:20:33 -0700

    homepage so blog posts dont show on front page

--- frontity.settings.js
+++ frontity.settings.js
@@ -17,7 +17,8 @@ const settings = {
         "source": {
           "api": "https://gph.fxx.mybluehost.me/wp-json",
           "url": "https://gph.fxx.mybluehost.me/",
-          "postsPage": "/blog"
+          "postsPage": "/blog",
+          "homepage": "/homepage", // seems like this fixed it
         }
       }
     },

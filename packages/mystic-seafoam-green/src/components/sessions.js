71218ca8f6f6a4ad2f03af328a4d9417050f21b8
Rachel Moore <rachel@rachelmoore.net>
2023-07-22 10:38:17 -0700

    mobile and medium sessions perfect

--- packages/mystic-seafoam-green/src/components/sessions.js
+++ packages/mystic-seafoam-green/src/components/sessions.js
@@ -11,7 +11,8 @@ import {
   PopoverTrigger,
   PopoverContent,
   Box,
-  Button
+  Button,
+  useMediaQuery
 } from '@chakra-ui/react';
 import { InlineWidget } from "react-calendly";
 import Contact from "./contact";
@@ -25,46 +26,10 @@ import racheljupiterskirt1000 from "../assets/carousel/racheljupiterskirt1000.jp
 import racheljupiterskirt1200 from "../assets/carousel/racheljupiterskirt1200.jpg";
 
 function Sessions() {
+    const [isLargerThan768] = useMediaQuery('(min-width: 768px)')
     return (
-        // <Flex direction="column">
-        // <Flex 
-        //     direction={{
-        //         base: "column",
-        //         lg: "row",
-        //     }} 
-        //     width="100%" 
-        //     height="100%"
-        // >
-        //     <Flex 
-        //         direction="column" 
-        //         width={{base: "100%", lg: "50%"}} 
-        //         padding={{base: 5, lg: 20}} 
-        //         borderRight={{base: "0px solid #000000", md: "2px solid #000000"}}
-        //     >
-        //         <Heading size="2xl" mt={10} mb={10} color="blackAlpha.800">Book a Photoshoot</Heading>
-                // <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
-                //     Portrait sessions are available for individuals, couples, and families. The shoot lasts for one hour and costs $350.
-                // </Text>
-
-                // <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
-                //     20 edited photos will be given to you within one week. You can also have all of the unedited photos if you would like them.
-                // </Text>
-
-                // <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
-                //     Alexandre will email you upon booking to determine where to meet for the shoot. If you are undecided but have a vision for what you hope the photos to capture he can help to select a location.
-                // </Text>
-        //     </Flex>
-
-        //     <Flex id="calendly" direction="column" width={{base: "100%", lg: "50%"}} padding={{base: 5, lg: 20}} bg="brand.700">
-        //         <CalendlyWrapper>
-        //             <InlineWidget id="calendly-widget" url="https://calendly.com/alexandreedgar/portrait-session"/>
-        //         </CalendlyWrapper>
-        //     </Flex>
-        // </Flex>
-        // <Flex direction="column" align="center" bg="black">
-        //   <Contact />
-        // </Flex>
-        // </Flex>
+        <>
+        {isLargerThan768 &&
         <Flex minHeight="85vh" pt={1} pb={1} bg="black" directon="column" alignItems="center" justifyContent="center">
             <Grid
                 minHeight='85vh'
@@ -74,10 +39,10 @@ function Sessions() {
                 bg="black"
                 maxWidth="1500px"
             >
-                <GridItem colSpan={3} rowSpan={2} backgroundImage={rachelmain600} backgroundPosition="center" backgroundRepeat="no-repeat">
+                <GridItem colSpan={{base: 4, md: 3}} rowSpan={2} backgroundImage={rachelmain600} backgroundPosition="center" backgroundRepeat="no-repeat">
                     <SessionsModal image={rachelmain600} />
                 </GridItem>
-                <GridItem colSpan={5} rowSpan={2} backgroundImage={racheljupiterskirt1000} backgroundPosition="center" backgroundRepeat="no-repeat">
+                <GridItem colSpan={{base: 4, md: 5}} rowSpan={2} backgroundImage={racheljupiterskirt1000} backgroundPosition="center" backgroundRepeat="no-repeat">
                     <SessionsModal image={racheljupiterskirt1000} />
                 </GridItem>
                 <GridItem colSpan={5} rowSpan={1} bg='brand.700'>
@@ -107,7 +72,116 @@ function Sessions() {
                     <SessionsModal image={rachelpool800} />
                 </GridItem>
             </Grid>
-        </Flex>
+            </Flex>
+        
+            }
+            {!isLargerThan768 &&
+                <Flex direction="column" minHeight="85vh" pt={1} pb={1} bg="black">
+
+                    <Flex 
+                        direction={{
+                            base: "column",
+                            sm: "row",
+                        }} 
+                    >
+                        <Flex 
+                            direction="column" 
+                            width={{base: "100%", sm: "50%"}} 
+                            height="600px"
+                            borderRight={{base: "0px solid #000000", sm: "4px solid #000000"}}
+                            backgroundImage={rachelmain600} 
+                            backgroundPosition="center" 
+                            backgroundRepeat="no-repeat"
+                        >
+                            <SessionsModal image={rachelmain600} />
+                        </Flex>
+                        <Flex 
+                            direction="column" 
+                            width={{base: "100%", sm: "50%"}} 
+                            height="600px"
+                            borderTop={{base: "4px solid #000000", sm: "0px solid #000000"}}
+                            backgroundImage={racheljupiterskirt1000} 
+                            backgroundPosition="center" 
+                            backgroundRepeat="no-repeat"
+                        >
+                            <SessionsModal image={racheljupiterskirt1000} />
+                        </Flex>
+                    </Flex>
+
+                    <Flex 
+                        direction={{
+                            base: "column",
+                            sm: "row",
+                        }} 
+                    >
+                        <Flex 
+                            direction="column" 
+                            width={{base: "100%", sm: "50%"}} 
+                            height={{base: "fit-content", sm: "900px"}}
+                            borderRight={{base: "0px solid #000000", sm: "4px solid #000000"}}
+                            borderTop="4px solid #000000"
+                            bg="brand.700"
+                        >
+                            <Flex height="100%" p={8} direction="column" alignItems="center" justifyContent="center">
+                                <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
+                                    Portrait sessions are available for individuals, couples, and families. The shoot lasts for one hour and costs $350.
+                                </Text>
+
+                                <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
+                                    20 edited photos will be given to you within one week. You can also have all of the unedited photos if you would like them.
+                                </Text>
+
+                                <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
+                                    Alexandre will email you upon booking to determine where to meet for the shoot. If you are undecided but have a vision for what you hope the photos to capture he can help to select a location.
+                                </Text>
+                            </Flex>
+                        </Flex>
+                        <Flex 
+                            direction="column" 
+                            width={{base: "100%", sm: "50%"}} 
+                            height="900px"
+                            borderTop="4px solid #000000"
+                        >
+                            <CalendlyWrapper>
+                                <InlineWidget id="calendly-widget" url="https://calendly.com/alexandreedgar/portrait-session"/>
+                            </CalendlyWrapper>
+                        </Flex>
+                    </Flex>
+
+                    <Flex 
+                        direction={{
+                            base: "column",
+                            sm: "row",
+                        }} 
+                    >
+                        <Flex 
+                            direction="column" 
+                            width={{base: "100%", sm: "50%"}} 
+                            height="500px"
+                            borderRight={{base: "0px solid #000000", sm: "4px solid #000000"}}
+                            borderTop="4px solid #000000"
+                            backgroundImage={rachelbeach800}
+                            backgroundPosition="center" 
+                            backgroundRepeat="no-repeat"
+                        >
+                            <SessionsModal image={rachelbeach800} />
+                        </Flex>
+                        <Flex 
+                            direction="column" 
+                            width={{base: "100%", sm: "50%"}} 
+                            height="500px"
+                            borderTop="4px solid #000000"
+                            backgroundImage={rachelpool800} 
+                            backgroundPosition="center" 
+                            backgroundRepeat="no-repeat"
+                        >
+                            <SessionsModal image={rachelpool800} />
+                        </Flex>
+                    </Flex>
+
+                </Flex>
+            }
+    </>
     )
   }
 
@@ -116,7 +190,12 @@ function Sessions() {
   const CalendlyWrapper = styled.div`
     height: 85vh;
     overflow: scroll;
+    background-color: #FFFFFF;
   iframe {
     border-radius: 170px;
   }
+  @media (max-width: 768px) {
+    height: 900px;
+  }
 `
+// colSpan={{base: 4, md: 5}} rowSpan={1}

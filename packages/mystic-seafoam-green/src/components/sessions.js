b53fced91537d7663a25dcd0404e03fae5553ab6
Rachel Moore <rachel@rachelmoore.net>
2023-07-22 09:45:42 -0700

    lint

--- packages/mystic-seafoam-green/src/components/sessions.js
+++ packages/mystic-seafoam-green/src/components/sessions.js
@@ -66,48 +66,47 @@ function Sessions() {
         // </Flex>
         // </Flex>
         <Flex minHeight="85vh" pt={1} pb={1} bg="black" directon="column" alignItems="center" justifyContent="center">
-        <Grid
-            minHeight='85vh'
-            templateRows='repeat(4, 1fr)'
-            templateColumns='repeat(8, 1fr)'
-            gap={1}
-            bg="black"
-            maxWidth="1500px"
-        >
+            <Grid
+                minHeight='85vh'
+                templateRows='repeat(4, 1fr)'
+                templateColumns='repeat(8, 1fr)'
+                gap={1}
+                bg="black"
+                maxWidth="1500px"
+            >
+                <GridItem colSpan={3} rowSpan={2} backgroundImage={rachelmain600} backgroundPosition="center" backgroundRepeat="no-repeat">
+                    <SessionsModal image={rachelmain600} />
+                </GridItem>
+                <GridItem colSpan={5} rowSpan={2} backgroundImage={racheljupiterskirt1000} backgroundPosition="center" backgroundRepeat="no-repeat">
+                    <SessionsModal image={racheljupiterskirt1000} />
+                </GridItem>
+                <GridItem colSpan={5} rowSpan={1} bg='brand.700'>
+                    <Flex height="100%" p={8} direction="column" alignItems="center" justifyContent="center">
+                        <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
+                            Portrait sessions are available for individuals, couples, and families. The shoot lasts for one hour and costs $350.
+                        </Text>
 
-            <GridItem colSpan={3} rowSpan={2} backgroundImage={rachelmain600} backgroundPosition="center" backgroundRepeat="no-repeat">
-                <SessionsModal image={rachelmain600} />
-            </GridItem>
-            <GridItem colSpan={5} rowSpan={2} backgroundImage={racheljupiterskirt1000} backgroundPosition="center" backgroundRepeat="no-repeat">
-                <SessionsModal image={racheljupiterskirt1000} />
-            </GridItem>
-            <GridItem colSpan={5} rowSpan={1} bg='brand.700'>
-                <Flex height="100%" p={8} direction="column" alignItems="center" justifyContent="center">
-                    <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
-                        Portrait sessions are available for individuals, couples, and families. The shoot lasts for one hour and costs $350.
-                    </Text>
+                        <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
+                            20 edited photos will be given to you within one week. You can also have all of the unedited photos if you would like them.
+                        </Text>
 
-                    <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
-                        20 edited photos will be given to you within one week. You can also have all of the unedited photos if you would like them.
-                    </Text>
-
-                    <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
-                        Alexandre will email you upon booking to determine where to meet for the shoot. If you are undecided but have a vision for what you hope the photos to capture he can help to select a location.
-                    </Text>
-                </Flex>
-            </GridItem>
-            <GridItem rowSpan={3} colSpan={3} bg='white'>
-                <CalendlyWrapper>
-                    <InlineWidget id="calendly-widget" url="https://calendly.com/alexandreedgar/portrait-session"/>
-                </CalendlyWrapper>
-            </GridItem>
-            <GridItem colSpan={3} rowSpan={1} backgroundImage={rachelbeach800} backgroundPosition="center" backgroundRepeat="no-repeat">
-                <SessionsModal image={rachelbeach800} />
-            </GridItem>
-            <GridItem colSpan={2} rowSpan={1} backgroundImage={rachelpool800} backgroundPosition="center" backgroundRepeat="no-repeat">
-                <SessionsModal image={rachelpool800} />
-            </GridItem>
-        </Grid>
+                        <Text fontWeight={500} mb={5} fontSize="lg" color="blackAlpha.800">
+                            Alexandre will email you upon booking to determine where to meet for the shoot. If you are undecided but have a vision for what you hope the photos to capture he can help to select a location.
+                        </Text>
+                    </Flex>
+                </GridItem>
+                <GridItem rowSpan={3} colSpan={3} bg='white'>
+                    <CalendlyWrapper>
+                        <InlineWidget id="calendly-widget" url="https://calendly.com/alexandreedgar/portrait-session"/>
+                    </CalendlyWrapper>
+                </GridItem>
+                <GridItem colSpan={3} rowSpan={1} backgroundImage={rachelbeach800} backgroundPosition="center" backgroundRepeat="no-repeat">
+                    <SessionsModal image={rachelbeach800} />
+                </GridItem>
+                <GridItem colSpan={2} rowSpan={1} backgroundImage={rachelpool800} backgroundPosition="center" backgroundRepeat="no-repeat">
+                    <SessionsModal image={rachelpool800} />
+                </GridItem>
+            </Grid>
         </Flex>
     )
   }
@@ -121,6 +120,3 @@ function Sessions() {
     border-radius: 170px;
   }
 `
-// backgroundImage={rachelmain800}
-// backgroundPosition="center"
-// backgroundRepeat="no-repeat"

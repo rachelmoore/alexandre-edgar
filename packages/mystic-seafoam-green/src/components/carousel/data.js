ca2f27f3a3a2b24a6a97a539a1587691db2ecbfb
Rachel Moore <rachel@rachelmoore.net>
2023-07-21 22:11:38 -0700

    correct photos

--- packages/mystic-seafoam-green/src/components/carousel/data.js
+++ packages/mystic-seafoam-green/src/components/carousel/data.js
@@ -10,81 +10,67 @@ import teacup from "../../assets/carousel/teacup.jpg"
 import vichytree from "../../assets/carousel/vichytree.jpg"
 import vineyard from "../../assets/carousel/vineyard.jpg"
 
+import truckredwoods from "../../assets/carousel/truckredwoods.jpg"
+import redtruck from "../../assets/carousel/redtruck.jpg"
+import windowflowers from "../../assets/carousel/windowflowers.jpg"
+import pole from "../../assets/carousel/pole.jpg"
+import landing from "../../assets/carousel/landing.jpg"
+import beachhouse from "../../assets/carousel/beachhouse.jpg"
+
 export const images = [
     {
       id: '01',
+      src: truckredwoods,
+      alt: 'Red Truck in Mendocino Redwoods',
+      aspectRatio: 4 / 3, 
+      objectFit: "contain !important",
+    },
+    {
+      id: '02',
       src: beaune,
       alt: 'Beaune',
       aspectRatio: 4 / 3, 
       objectFit: "contain !important",
     },
     {
-      id: '02',
-      src: bwtree,
-      alt: 'Black and white tree',
-      aspectRatio: 4 / 3, 
-      objectFit: "contain !important",
-    },
-    {
       id: '03',
-      src: crow,
-      alt: 'Crow',
-      aspectRatio: 4 / 3, 
-      objectFit: "cover",
-    },
-    {
-      id: '04',
-      src: goldengatebridge,
-      alt: 'Golden Gate Bridge',
-      aspectRatio: 4 / 3, 
-      objectFit: "contain !important",
-    },
-    {
-      id: '05',
-      src: kitten,
-      alt: 'Kitten',
-      aspectRatio: 4 / 3, 
-      objectFit: "contain !important",
-    },
-    {
-      id: '06',
-      src: oceanrocks,
-      alt: 'Ocean rocks',
-      aspectRatio: 4 / 3, 
-      objectFit: "contain !important",
-    },
-    {
-      id: '07',
-      src: peacock,
-      alt: 'Peacock',
-      aspectRatio: 4 / 3, 
-      objectFit: "contain !important",
-    },
-    {
-      id: '08',
-      src: ruby,
-      alt: 'Cat with grass',
-      aspectRatio: 4 / 3, 
-      objectFit: "cover",
-    },
-    {
-      id: '09',
       src: teacup,
       alt: 'Teacup',
       aspectRatio: 4 / 3, 
       objectFit: "contain !important",
     },
     {
-      id: '10',
-      src: vichytree,
-      alt: 'Vichy Springs tree',
+      id: '04',
+      src: windowflowers,
+      alt: 'Orange flowers in Albion window',
       aspectRatio: 4 / 3, 
       objectFit: "contain !important",
     },
     {
-      id: '11',
-      src: vineyard,
-      alt: 'Ukiah vineyard',
+      id: '05',
+      src: redtruck,
+      alt: 'Red Truck in Fort Bragg',
+      aspectRatio: 4 / 3, 
+      objectFit: "contain !important",
+    },
+    {
+      id: '06',
+      src: pole,
+      alt: 'Telephone pole on Hwy 1 California',
+      aspectRatio: 4 / 3, 
+      objectFit: "contain !important",
+    },
+    {
+      id: '07',
+      src: landing,
+      alt: "Bridgeport Landing",
+      aspectRatio: 4 / 3, 
+      objectFit: "contain !important",
+    },
+    {
+      id: '08',
+      src: beachhouse,
+      alt: 'House in Manchester California',
       aspectRatio: 4 / 3, 
       objectFit: "cover",
     },
